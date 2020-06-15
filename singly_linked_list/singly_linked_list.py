979b13677cdb80bb3de8d4e3d0af7f48bab72c04
Matt Gill <matt@dustcloudlabs.com>
2020-06-15 18:17:26 -0400

    started Singly linked list, queue

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,18 @@
+class ListNode: 
+    def __init__(self,value,next):
+        self.value = value
+        self.next = next
+    
+    def insert(self,value):
+        pass
+
+    def delete(self):
+        pass
+
+
+class SingleList:
+    def __init_(self,node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+    
