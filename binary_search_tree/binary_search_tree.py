ac454fc966f550bd700d3364f91eba020d36030a
Matt Gill <matt@dustcloudlabs.com>
2020-06-24 19:12:26 -0400

    in_order_print passing test

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -72,7 +72,12 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if self.left:
+            self.left.in_order_print(self.left)
+        print(self.value)
+        if self.right:
+            self.right.in_order_print(self.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
